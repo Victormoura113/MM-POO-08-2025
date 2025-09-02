5e05b2a210056acffedad38145566beb8e861a72
Victormoura113 <victormoura113@gmail.com>
2025-09-02 00:32:36 +0000

    feat: add projeto

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+Televisao tv = new Televisao(70f);
+
+Console.WriteLine($"A tv tem o tamanho {tv.Tamanho}");
+
+tv.Volume = 100;
+console.WriteLine($"volume {tv.Volume}");
